4379d06af7f2a678100a49732b009950c9b45ce7
Maxim Donchuk <m.donchuk@gmail.com>
2024-01-26 13:57:51 +0300

    show_recipes_without_product dublicatet to root url

--- recipes/urls.py
+++ recipes/urls.py
@@ -8,5 +8,6 @@ app_name = RecipesConfig.name
 urlpatterns = [
     path("add_product_to_recipe/", views.add_product_to_recipe, name="add_product_to_recipe"),
     path("cook_recipe/", views.cook_recipe, name="cook_recipe"),
-    path("show_recipes_without_product", views.RecipeListView.as_view(), name="show_recipes_without_product")
+    path("show_recipes_without_product", views.RecipeListView.as_view(), name="show_recipes_without_product"),
+    path("", views.RecipeListView.as_view(), name="show_recipes_without_product")
 ]
