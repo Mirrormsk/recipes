6313bb9898af8247c0dde4bfe570559b950e3e5a
Maxim Donchuk <m.donchuk@gmail.com>
2024-01-26 11:04:04 +0300

    models registered in admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from recipes.models import Recipe, Product, Ingredient
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'usage_count']
+    fields = ['title', 'usage_count']
+    readonly_fields = ['usage_count']
+    search_fields = ['title']
+    ordering = ['-usage_count']
+
+
+class IngredientInlineAdmin(admin.TabularInline):
+    model = Ingredient
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    fields = ['title']
+    inlines = [IngredientInlineAdmin]
+    search_fields = ['title']
+    ordering = ['-pk']
+
